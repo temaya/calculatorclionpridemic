724a69488f55754a48a8c85b6fcdaa766882d3f1
Minh Tuan Nguyen <tuan@Tuans-MBP.local>
2023-02-10 16:10:41 +0100

    feat(WEVOICIFY-305 Create the code skeleton):
    initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+//create a calculator app
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << num1+num2 << endl;
+            break;
+
+        case '-':
+            cout << num1-num2 << endl;
+            break;
+
+        case '*':
+            cout << num1*num2 << endl;
+            break;
+
+        case '/':
+            cout << num1/num2 << endl;
+            break;
+
+        default:
+
+            cout << "Error! operator is not correct";
+
+            break;  }  return 0;}
