af0e5757aec0c306d946f71087278691df7edf04
datics consulting <project@datics-consulting.com>
2023-02-15 16:24:36 +0700

    refactor(WEVOICIFY-305 Refactor main code)
    Rewrite calculator function into library

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include "calculator.h"
+
+double Calculator::add(double a, double b) {
+    return a + b;
+}
+
+double Calculator::subtract(double a, double b) {
+    return a - b;
+}
+
+double Calculator::multiply(double a, double b) {
+    return a * b;
+}
+
+double Calculator::divide(double a, double b) {
+    if (b == 0) {
+        throw std::invalid_argument("division by zero");
+    }
+    return a / b;
+}
---
+++ calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+#include <stdexcept>
+
+class Calculator {
+public:
+    double add(double a, double b);
+    double subtract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
+};
+
+#endif
